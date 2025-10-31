9a27032b610e8c0a65d2bc4c8b9fa4cec8faa05c
mroch4 <mroch4@users.noreply.github.com>
2025-10-31 15:40:13 +0000

    deploy: 7014e279e2d9145a0f3bfb93d36823a2bb7c5f26

---
+++ js/leaflet.js
@@ -0,0 +1,35 @@
+﻿window.leafletMapInterop = {
+    zoom: 13,
+
+    init: function (latitude, longitude, entries, dotNetObjectReference) {
+        this.map = L.map("map", {
+            center: [latitude, longitude],
+            zoom: this.zoom,
+        });
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(this.map);
+
+        entries.forEach((entry) =>
+
+            L.marker([entry.b.a, entry.b.b], {
+
+                icon: L.icon({
+                    iconSize: [25, 41],
+                    iconAnchor: [13, 41],
+                    iconUrl: "icons/marker.png",
+                    popupAnchor: [-1, -10],
+                }),
+            })
+                .addTo(this.map)
+                .bindPopup(`<b>${entry.e}</b>`)
+                .on("click", () => {
+                    this.map.setView([entry.b.a, entry.b.b], this.map.zoom);
+                    dotNetObjectReference.invokeMethodAsync("OnMarkerOpened", entry.a);
+                })
+                .on("popupclose", () => {
+                    this.map.setView([latitude, longitude], this.zoom);
+                    dotNetObjectReference.invokeMethodAsync("OnMarkerClosed");
+                })
+        );
+    },
+};
